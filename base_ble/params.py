53e9abba2ac41a14d0ddbc9997d869542d7d1a9f
MorganStrauss <31320545+MorganStrauss@users.noreply.github.com>
2024-05-21 15:31:40 -0400

    Add files via upload
    Updated params file to include offset values and gains to calibrate the SmartHub units.

--- base_ble/params.py
+++ base_ble/params.py
@@ -1,10 +1,15 @@
 import os
 from datetime import datetime
 
+left_gain = 1.13
+left_offset = -0.049
+right_gain = 1.12
+right_offset = -0.0357
+
 D_EULER_THRESH = 25
 WHEEL_DIAM_IN = 23  # 22
 IN_TO_M = 0.0254
-DIST_WHEELS_IN = 26  # 20.5
+DIST_WHEELS_IN = 20  # 26
 
 DATETIME_FMT = '%Y%m%d'
 DATETIME_HMS_FMT = '%Y%m%d-%H%M%S'
