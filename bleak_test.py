a4d4e691606bcd4ff0ff0274514310daed6061e3
nwaltz <nwaltz01@gmail.com>
2025-01-25 17:54:28 -0500

    rssi vals

--- bleak_test.py
+++ bleak_test.py
@@ -2,32 +2,37 @@ import asyncio
 from bleak import BleakClient, BleakScanner
 from bleak.exc import BleakError
 
-address = "9D:C3:CE:33:0F:13"
+address = "A32DB278-7D5F-3CFF-433E-F1C464A46137"
 
-def update_data(ch, data, side):
-    print(f"{side} Smarthub: {data}")
 
 async def connect_to_device():
     try:
+        notified = False
         async with BleakClient(address) as client:
             while True:
-                if await client.is_connected():
-                    print("Device is connected")
+                if not notified:
+                    notified = True
                     ch = "00002a56-0000-1000-8000-00805f9b34fb"
+                    await client.start_notify(ch, lambda ch, data: print(data))
 
-                    await client.start_notify(ch, lambda ch, data: update_data(ch, data, 'left'))
-                else:
-                    print("Device is not connected")
-                # await asyncio.sleep(5)  # Wait a few seconds before checking again
+                # else:
+                #     print("Device is not connected")
+                await asyncio.sleep(5)  # Wait a few seconds before checking again
+                rssi = await client.get_rssi()
+                print(f"RSSI: {rssi}")
 
     except BleakError as e:
         print(f"Failed to connect: {e}")
 
 async def main():
-    devices = await BleakScanner.discover(timeout=5.0)
+    devices = await BleakScanner.discover(timeout=5.0, return_adv=True)
     smarthub_id = "9999"
-    for d in devices:
-        print(d)
+    # print(devices)
+    for d, val in devices.items():
+        device, adv = val
+        if not "None" in str(device.name):
+            print(f"Name: {device.name: <40}|     RSSI (signal strength): {adv.rssi: <10}|       Address: {d}")
+            print()
     #     if isinstance(d.name, str):
     #         if f'Left Smarthub: {smarthub_id}' == d.name:
     #             print("Left Smarthub Identified")
@@ -42,5 +47,5 @@ async def main():
     #         print("Device went out of range, retrying...")
     #         await asyncio.sleep(10)  # Wait before retrying
 
-# asyncio.run(main())
-asyncio.run(connect_to_device())
+asyncio.run(main())
+# asyncio.run(connect_to_device())
